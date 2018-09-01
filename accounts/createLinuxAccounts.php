2c32aa775b701bc7190e67a260fad46163d8e306
Louis-Henri Merino <lhmerino@terpmail.umd.edu>
2018-09-01 11:10:59 -0400

    Linux Accounts

--- accounts/createLinuxAccounts.php
+++ accounts/createLinuxAccounts.php
@@ -1,19 +1,20 @@
 <?php
 
-if(!isset($argv[1]))
+if(!isset($argv[1]) && !isset($argv[2]))
 {
-	echo 'Usage: php '.$argv[0].' <path_to_file>'.PHP_EOL;
+	echo 'Usage: php '.$argv[0].' <path_to_file> <default_password>'.PHP_EOL;
 	exit(-1);
 }
 
 $file = $argv[1];
+$password = $argv[2];
 
 $handle = fopen($file, "r");
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
 	$line = str_replace("\n", "", $line); //remove new line character
     	exec('useradd -m -s /bin/bash '.$line);
-	exec('echo "'.$line.':ACES@12345" | chpasswd');
+	exec('echo "'.$line.':'.$password.'" | chpasswd');
 	echo 'Created account: '.$line.PHP_EOL;
     }
 
